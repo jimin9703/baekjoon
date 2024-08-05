c185300cca9f32c2ca59980483c859bd5f1cdb8e
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-08-05 15:20:55 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+if n >= 90:
+    print('A')
+elif n >= 80 and n < 90:
+    print('B')
+elif n >= 70 and n < 80:
+    print('C')
+elif n >= 60 and n < 70:
+    print('D')
+else:
+    print('F')
