3252e7bc8e010e45703beb93b5fb84c4e360bebe
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-08-29 15:08:59 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -2,19 +2,15 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 
-num = list(map(int, sys.stdin.readline().strip().split()))
-count = 0
+m = list(map(int, sys.stdin.readline().strip().split()))
+cnt = 0
 
-for i in num:
-    if i == 1:
-        continue
+for i in range(len(m)):
+    L = []
+    for j in range(1,m[i]+1):
+        if not m[i] % j:
+            L.append(i)
+    if len(L) == 2:
+        cnt += 1
 
-    for j in range(2, i):
-        
-        if i % j == 0:
-            break
-    else:
-        count += 1
-
-           
-print(count)
+print(cnt)
