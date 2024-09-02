edb697a31015604e918e07c9c8f4724b29cfc31e
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-09-02 18:03:09 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -4,13 +4,6 @@ a, b = map(int, sys.stdin.readline().strip().split())
 c = int(sys.stdin.readline().strip())
 d = int(sys.stdin.readline().strip())
 
-# if c-a <= 0:
-#     print(0)
-# else:
-#     if d >= b / (c-a):
-#         print(1)
-#     else:
-#         print(0)
 if a*d + b <= c*d and c >= a:
     print(1)
 else:
