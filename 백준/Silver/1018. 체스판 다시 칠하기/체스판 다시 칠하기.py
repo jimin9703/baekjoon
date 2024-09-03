307607666a32710834cbbb92f628719b87520c00
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-09-03 15:01:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,25 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().strip().split())
+
+L = [sys.stdin.readline().strip() for _ in range(n)]
+
+W = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+B = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+
+
+ans = 2501
+
+for i in range(n-7):
+    for j in range(m-7):
+        cnt = 0
+        cnt2 = 0
+        for k in range(8):
+            for l in range(8):                    
+                if L[i+k][j+l] != W[k][l]:
+                    cnt += 1
+                if L[i+k][j+l] != B[k][l]:
+                    cnt2 += 1
+        ans = min(cnt, cnt2, ans)
+
+print(ans)
