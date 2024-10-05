3b920fff062ad97ae5b7abe16c430129c9fd5477
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-10-05 14:41:15 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,25 +3,26 @@ from collections import deque
 
 input = sys.stdin.readline
 n, m = map(int, input().strip().split())
-graph = list(input().strip() for _ in range(n))
-visited = [[-1] * m for _ in range(n)]
+graph = [input().strip() for _ in range(n)]
+visited = [[-1]*m for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
 q = deque()
-q.append((0,0))
-visited[0][0]=1
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+q.append((0, 0))
+visited[0][0] = 1
 
 while q:
-    qx, qy = q.popleft()
+    x, y = q.popleft()
+
     for i in range(4):
-        nx, ny = qx + dx[i], qy + dy[i]
-        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+        nx, ny = x+dx[i], y+dy[i]
+        if nx<0 or ny<0 or nx>=n or ny>= m:
             continue
         if graph[nx][ny] == "0":
             continue
         if visited[nx][ny] != -1:
             continue
-        visited[nx][ny] = visited[qx][qy] + 1
+        visited[nx][ny] = visited[x][y] + 1
         q.append((nx, ny))
-        
+
 print(visited[n-1][m-1])
