4f2310dc7f031c99c340745148360e08940ce177
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-10-11 23:31:01 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 576 ms, Memory: 37600 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+INF= int(9e18)
+q = deque()
+
+n, k = map(int, input().strip().split())
+visited = [INF] * 100001
+q.append(n)
+visited[n] = 0
+cnt = 0
+ans = []
+
+while q:
+    x = q.popleft()
+
+    for i in [x*2, x+1, x-1]:
+        if 0<=i and i<=100000:
+            if visited[i] >= visited[x]+1:
+                visited[i] = visited[x]+1
+                q.append(i)
+
+        if i == k:
+            if visited[i] >= visited[x]+1:
+                cnt += 1
+                # ans.append(visited[i])
+
+
+print(visited[k])
+# for i in ans:
+#     if i == visited[k]:
+#         cnt += 1
+
+if n == k:
+    cnt = 1
+print(cnt)
