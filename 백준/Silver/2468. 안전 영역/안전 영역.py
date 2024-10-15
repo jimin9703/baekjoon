1a7fb238ab3e1c7fb0d198f4e40436902316eee4
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-10-15 20:25:53 +0900

    [Silver I] Title: 안전 영역, Time: 632 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -22,37 +22,50 @@ for i in range(n):
 rain = 0
 
 
-def BFS(start, rain):
-    q.append(start)
-    while q:
-        x, y = q.popleft()
-        visited[x][y] = 1
-
-        for i in range(4):
-            nx, ny = x+dx[i], y+dy[i]
-            if nx<0 or ny<0 or nx>=n or ny>=n:
-                continue
-            if visited[nx][ny] != -1:
-                continue
-            if graph[nx][ny] <= rain:
-                continue
-            visited[nx][ny] = 1
-            q.append((nx, ny))
-            
-ans = 0
-while 1:
-    if rain >= max_v:
-        break
+def BFS(rain):
     danji = 0
     visited = [[-1]*n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if graph[i][j] > rain and visited[i][j] == -1:
-                BFS((i,j), rain)
+                q.append((i, j))
+                visited[i][j] = 1
+
+                while q:
+                    x, y = q.popleft()
+                    
+
+                    for k in range(4):
+                        nx, ny = x+dx[k], y+dy[k]
+                        if nx<0 or ny<0 or nx>=n or ny>=n:
+                            continue
+                        if visited[nx][ny] != -1:
+                            continue
+                        if graph[nx][ny] <= rain:
+                            continue
+                        visited[nx][ny] = 1
+                        q.append((nx, ny))
                 danji += 1
+    
+    
+            
+    return danji
+
+ans = 0
+while 1:
+    danji = BFS(rain)
+    if rain >= max_v:
+        break
+    
+    
+    
 
     if ans < danji:
         ans = danji
     rain += 1
 
 print(ans)
+
+
+
+
