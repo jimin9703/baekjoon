38826487159b685de3f746093ae39b4f6cf2843d
jimin9703 <168407671+jimin9703@users.noreply.github.com>
2024-10-17 08:48:34 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def solution(n, computers):
+    q = deque()
+    visited = [-1] * n
+    # for i in range(1, n):
+    #     if computers[0][i] == 1:
+    #         q.append(i)
+    #         visited[i] = 1
+    q.append(0)
+    visited[0] = 1
+    cnt = 0
+    if not q:
+        cnt = n
+    while q:
+        x = q.popleft()
+        
+        
+        for i in range(1, n):
+            if i == x:
+                continue
+            if visited[i] != -1:
+                continue
+            if computers[x][i] == 1:
+                q.append(i)
+                visited[i] = 1
+                
+        if not q:
+            cnt += 1
+            for i in range(n):
+                if visited[i] == -1:
+                    q.append(i)
+                    visited[i] = 1
+                    break
+    answer = cnt
+    return answer
